c8493ff899019d12eb5ed2ff093bba8ea8892242
ha1f <half.ldk.hiz@gmail.com>
2017-11-03 14:55:45 +0900

    Refactor

--- MusubiImagePicker/CheckBoxView.swift
+++ MusubiImagePicker/CheckBoxView.swift
@@ -13,65 +13,61 @@ struct CheckBoxViewColors {
     let ovalFrameColor: UIColor
     let checkColor: UIColor
     
-    static let green = CheckBoxViewColors(
+    static let defaultActiveColor = CheckBoxViewColors(
         ovalColor: UIColor(red: 85/255, green: 185/255, blue: 1/255, alpha: 0.8),
         ovalFrameColor: UIColor.black,
         checkColor: UIColor.white
     )
-    static let gray = CheckBoxViewColors(
+    static let defaultNegativeColor = CheckBoxViewColors(
         ovalColor: UIColor(red: 150/255, green: 150/255, blue: 150/255, alpha: 0.2),
         ovalFrameColor: UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 0.3),
         checkColor: UIColor(red: 1, green: 1, blue: 1, alpha: 0.5)
     )
 }
 
-// 参考：http://mashiroyuya.hatenablog.com/entry/2016/03/01/131211
+/// 参考：http://mashiroyuya.hatenablog.com/entry/2016/03/01/131211
 class CheckBoxView: UIView {
     
-    private var isSelected = false
+    var isActive = false {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
     
-    convenience init(frame: CGRect, selected: Bool) {
+    convenience init(frame: CGRect, isActive: Bool) {
         self.init(frame: frame)
-        self.isSelected = selected
+        self.isActive = isActive
         self.backgroundColor = UIColor.clear
     }
     
     override func draw(_ rect: CGRect) {
         let checkMarkRect = CGRect(x: 5, y: 5, width: rect.width - 10, height: rect.height - 10)
         
-        let checkBoxViewColors = isSelected ? CheckBoxViewColors.green : CheckBoxViewColors.gray
+        let checkBoxViewColors = isActive ? CheckBoxViewColors.defaultActiveColor : CheckBoxViewColors.defaultNegativeColor
         
-        // 円 -------------------------------------
+        // Draw Circle
         let oval = UIBezierPath(ovalIn: checkMarkRect)
-        
-        // 塗りつぶし色の設定
         checkBoxViewColors.ovalColor.setFill()
-        // 内側の塗りつぶし
         oval.fill()
-        //枠の色
         checkBoxViewColors.ovalFrameColor.setStroke()
-        //枠の太さ
         oval.lineWidth = 2
-        // 描画
         oval.stroke()
         
-        let xx = checkMarkRect.origin.x
-        let yy = checkMarkRect.origin.y
-        let width = checkMarkRect.width
-        let height = checkMarkRect.height
+        // Draw checkmark
+        let startPoint = CGPoint(x: checkMarkRect.origin.x + checkMarkRect.width / 6,
+                                 y: checkMarkRect.origin.y + checkMarkRect.height / 2)
+        let turnPoint = CGPoint(x: checkMarkRect.origin.x + checkMarkRect.width / 3,
+                                y: checkMarkRect.origin.y + checkMarkRect.height * 7 / 10)
+        let endPoint = CGPoint(x: checkMarkRect.origin.x + checkMarkRect.width * 5 / 6,
+                               y: checkMarkRect.origin.y + checkMarkRect.height * 1 / 3)
         
-        // チェックマークの描画 ----------------------
         let checkmark = UIBezierPath()
-        //起点
-        checkmark.move(to: CGPoint(x: xx + width / 6, y: yy + height / 2))
-        //帰着点
-        checkmark.addLine(to: CGPoint(x: xx + width / 3, y: yy + height * 7 / 10))
-        checkmark.addLine(to: CGPoint(x: xx + width * 5 / 6, y: yy + height * 1 / 3))
-        // 色の設定
+        checkmark.move(to: startPoint)
+        checkmark.addLine(to: turnPoint)
+        checkmark.addLine(to: endPoint)
+        
         checkBoxViewColors.checkColor.setStroke()
-        // ライン幅
-        checkmark.lineWidth = 6
-        // 描画
+        checkmark.lineWidth = 5
         checkmark.stroke()
     }
 }
--- MusubiImagePicker/MusubiGridViewCell.swift
+++ MusubiImagePicker/MusubiGridViewCell.swift
@@ -10,54 +10,67 @@ import UIKit
 
 class MusubiGridViewCell: GridViewCell {
     
-    private var checkBoxView: CheckBoxView!
+    static let selectedBackgroundColor = UIColor.green
+    static let whiteLayerColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.7)
+    
+    private lazy var checkBoxView: CheckBoxView = {
+        return CheckBoxView(frame: CGRect.zero, isActive: true)
+    }()
+    
     private var whiteLayer: CALayer?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupCheckBox()
+        _commonInit()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        setupCheckBox()
+        _commonInit()
+    }
+    
+    private func setupWhiteLayer() {
+        whiteLayer?.removeFromSuperlayer()
+        let layer = CALayer()
+        layer.frame = self.bounds
+        layer.backgroundColor = MusubiGridViewCell.whiteLayerColor.cgColor
+        self.layer.addSublayer(layer)
+        whiteLayer = layer
+    }
+    
+    private func removeWhiteLayer() {
+        whiteLayer?.removeFromSuperlayer()
+        whiteLayer = nil
     }
     
     override var isUserInteractionEnabled: Bool {
         didSet {
             if isUserInteractionEnabled {
-                whiteLayer?.removeFromSuperlayer()
-                whiteLayer = nil
+                removeWhiteLayer()
             } else {
-                whiteLayer?.removeFromSuperlayer()
-                let layer = CALayer()
-                layer.frame = self.bounds
-                layer.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.7).cgColor
-                self.layer.addSublayer(layer)
-                whiteLayer = layer
+                setupWhiteLayer()
             }
         }
     }
     
     override var isSelected: Bool {
         didSet {
-            checkBoxView.isHidden = !isSelected
+            _updateCheckbox()
             if oldValue != isSelected {
-                // 変化あり
-                let imageView = self.imageView
+                // 変化あったときのみアニメーション
                 if isSelected {
-                    UIView.animate(withDuration: 0.05, animations: { [weak imageView] in
+                    UIView.animate(withDuration: 0.05, animations: { [weak imageView = self.imageView] in
                         imageView?.transform = CGAffineTransform(scaleX: 0.93, y: 0.93)
                         }, completion: { _ in
-                            UIView.animate(withDuration: 0.03) {[weak imageView] in
+                            UIView.animate(withDuration: 0.03) {[weak imageView = self.imageView] in
                                 imageView?.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
                             }
                     })
                 } else {
-                    UIView.animate(withDuration: 0.04, animations: { [weak imageView] in
+                    UIView.animate(withDuration: 0.03, animations: { [weak imageView = self.imageView] in
                         imageView?.transform = CGAffineTransform(scaleX: 0.92, y: 0.92)
                         }, completion: { _ in
-                            UIView.animate(withDuration: 0.05) {[weak imageView] in
+                            UIView.animate(withDuration: 0.05) {[weak imageView = self.imageView] in
                                 imageView?.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
                             }
                     })
@@ -66,24 +79,31 @@ class MusubiGridViewCell: GridViewCell {
         }
     }
     
-    private func setupCheckBox() {
+    private func _updateCheckbox() {
+        checkBoxView.isActive = isSelected
+        checkBoxView.isHidden = !isSelected
+    }
+    
+    private func _commonInit() {
         let boxWidth = frame.width * 0.5
         let boxRect = CGRect(origin: .zero, size: CGSize(width: boxWidth, height: boxWidth))
         
-        checkBoxView = CheckBoxView(frame: boxRect, selected: true)
-        checkBoxView.isHidden = !isSelected
+        if !self.subviews.contains(checkBoxView) {
+            self.addSubview(checkBoxView)
+        }
+        
+        checkBoxView.frame = boxRect
+        _updateCheckbox()
         
         self.selectedBackgroundView = UIView(frame: frame)
-        selectedBackgroundView?.backgroundColor = UIColor.green
-        
-        self.addSubview(checkBoxView)
+        selectedBackgroundView?.backgroundColor = MusubiGridViewCell.selectedBackgroundColor
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
-        whiteLayer?.removeFromSuperlayer()
-        whiteLayer = nil
+        removeWhiteLayer()
         isUserInteractionEnabled = true
+        isSelected = false
     }
     
 }
